62ce9962c8b679ebcdcf1bcd17be6e45bdf8b126
yogesh <yogirao2906@gmail.com>
2022-08-17 18:33:17 +0530

    Updates

---
+++ precache-manifest.5272ef54a7b529266e3e3934019460f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058e1fe506486eb6e584750ca85b2780",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "3b9eed5126b90b47fb09",
+    "url": "/react-to-do-list/static/css/main.0b837af0.chunk.css"
+  },
+  {
+    "revision": "6ad41ec808f9f5cbcfb4",
+    "url": "/react-to-do-list/static/js/2.c057aba1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-to-do-list/static/js/2.c057aba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b9eed5126b90b47fb09",
+    "url": "/react-to-do-list/static/js/main.6f345f84.chunk.js"
+  },
+  {
+    "revision": "d22f819270a09239a853",
+    "url": "/react-to-do-list/static/js/runtime-main.c0e1d1a3.js"
+  }
+]);
